18526d117229d54625b5f0fa06ace17ef8cf87b1
Gabriela Pimenta <121987083+gab1pimenta@users.noreply.github.com>
2024-10-03 19:37:02 -0300

    Finalizado

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
-//primeiro código em JavaScript
+//Classificador de nível de Herói
 
-//Variável que armazena o nome do herói 
-let nomeHeroi = prompt("Qual é o nome do seu Herói? ")
+//Variável que armazena o nome do herói
+let nomeHeroi = "João"
+//Variável que armazena o XP
+let xp = 3000
+let nivel = " "
+
+if (xp < 1000) {
+    nivel = "Ferro"
+} else if (xp >= 1000 && xp <= 2000){
+    nivel = "Bronze"
+} else if (xp >= 2001 && xp < 5000){
+    nivel = "Prata"
+}else if (xp >= 5001 && xp < 7000){
+    nivel = "Ouro"
+}else if (xp >= 7001 && xp < 8000){
+    nivel = "Platina"
+}else if (xp >= 8001 && xp < 9000){
+    nivel = "Ascendente"
+}else if (xp >= 9001 && xp < 10000){
+    nivel = "Imortal"
+}else if (xp >= 10001){
+    nivel = "Radiante"
+}
+console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivel)
+
 
